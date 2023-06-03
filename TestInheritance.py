0ed84e270d1abb1b928bfa7fee96ac319d283691
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-06-03 16:49:16 +0800

    Run all operations and display results

--- TestInheritance.py
+++ TestInheritance.py
@@ -8,21 +8,23 @@ InCal = InheritCalculator()
 num1 = Inp.ask_user_input()
 num2 = Inp.ask_user_input()
 
-sum = InheritCalculator.addition(self= InheritCalculator, num1= num1, num2= num2)
+sum = InCal.addition(num1= num1, num2= num2)
 print("\n\033[92mThe sum of", num1, "and", num2, "is:", sum)
 
-diffence = InheritCalculator.subtraction(self= InheritCalculator, num1= num1, num2= num2)
+diffence = InCal.subtraction(num1= num1, num2= num2)
 print("\n\033[93mThe difference of", num1, "and", num2, "is:", diffence)
 
-product = InheritCalculator.multiplication(self= InheritCalculator, num1= num1, num2= num2)
+product = InCal.multiplication(num1= num1, num2= num2)
 print("\n\033[94mThe product of", num1, "and", num2, "is:", product)
 
-quotient = InheritCalculator.division(self= InheritCalculator, num1= num1, num2= num2)
+quotient = InCal.division(num1= num1, num2= num2)
 print("\n\033[95mThe quotient of", num1, "and", num2, "is:", quotient)
 
-power = InheritCalculator.power(self= InheritCalculator, num1= num1, num2= num2)
+power = InCal.power(num1= num1, num2= num2)
 print("\n\033[91mThe first number,", num1, "raised to the power of", num2, "is:", power)
 
-sr = InheritCalculator.square_root(self= InheritCalculator, num1= num1)
+sr = InCal.square_root(num1= num1)
 print("\n\033[96mThe Square root of",num1, "is:", sr)
 
+sr = InCal.square_root(num1= num2)
+print("\n\033[97mThe Square root of",num2, "is:", sr)
