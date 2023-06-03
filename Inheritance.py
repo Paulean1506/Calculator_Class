ec76d4a52963c6106a9736eae310971a9da258a7
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-06-03 20:09:58 +0800

    finish program by adding loop

--- Inheritance.py
+++ Inheritance.py
@@ -1,4 +1,5 @@
 from Class_Calculator import Calculator
+from User_Interface import UserInterface
 
 class InheritCalculator(Calculator):
     def power(self, num1, num2):
@@ -6,3 +7,16 @@ class InheritCalculator(Calculator):
     
     def square_root(self, num1):
         return num1 ** 0.5
+    
+class InheritUI(UserInterface):
+    def try_again(self):
+        while True:
+            TA = input("\n\033[96mDo you want to try again? 'y' or 'n': ")
+            if TA in ["y", "n"]:
+
+                if TA == 'y':
+                    continue
+
+            else:
+                print('\nThank you!')
+                break
--- TestInheritance.py
+++ TestInheritance.py
@@ -1,30 +1,40 @@
 from User_Interface import UserInterface
 from Inheritance import InheritCalculator
+from termcolor import colored
+from pyfiglet import Figlet
 
 Inp = UserInterface()
 InCal = InheritCalculator()
 
+while True:
+    num1 = Inp.ask_user_input()
+    num2 = Inp.ask_user_input()
 
-num1 = Inp.ask_user_input()
-num2 = Inp.ask_user_input()
+    sum = InCal.addition(num1= num1, num2= num2)
+    print("\n\033[92mThe sum of", num1, "and", num2, "is:", sum)
 
-sum = InCal.addition(num1= num1, num2= num2)
-print("\n\033[92mThe sum of", num1, "and", num2, "is:", sum)
+    diffence = InCal.subtraction(num1= num1, num2= num2)
+    print("\n\033[93mThe difference of", num1, "and", num2, "is:", diffence)
 
-diffence = InCal.subtraction(num1= num1, num2= num2)
-print("\n\033[93mThe difference of", num1, "and", num2, "is:", diffence)
+    product = InCal.multiplication(num1= num1, num2= num2)
+    print("\n\033[94mThe product of", num1, "and", num2, "is:", product)
 
-product = InCal.multiplication(num1= num1, num2= num2)
-print("\n\033[94mThe product of", num1, "and", num2, "is:", product)
+    quotient = InCal.division(num1= num1, num2= num2)
+    print("\n\033[95mThe quotient of", num1, "and", num2, "is:", quotient)
 
-quotient = InCal.division(num1= num1, num2= num2)
-print("\n\033[95mThe quotient of", num1, "and", num2, "is:", quotient)
+    power = InCal.power(num1= num1, num2= num2)
+    print("\n\033[91mThe first number,", num1, "raised to the power of", num2, "is:", power)
 
-power = InCal.power(num1= num1, num2= num2)
-print("\n\033[91mThe first number,", num1, "raised to the power of", num2, "is:", power)
+    sr = InCal.square_root(num1= num1)
+    print("\n\033[96mThe Square root of",num1, "is:", sr)
 
-sr = InCal.square_root(num1= num1)
-print("\n\033[96mThe Square root of",num1, "is:", sr)
+    sr = InCal.square_root(num1= num2)
+    print("\n\033[97mThe Square root of",num2, "is:", sr)
 
-sr = InCal.square_root(num1= num2)
-print("\n\033[97mThe Square root of",num2, "is:", sr)
+    try_again = input("\nDo you want to try again? 'y' or 'n': ")
+    if try_again.lower() == "n":
+        r = Figlet(font = "cybermedium")
+        print(colored(r.renderText('\nThank you!'), 'red'))
+        break
+        
+
