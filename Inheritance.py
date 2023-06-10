fa0a43a0b56c73f6ff08f7bfa54921914a2189ab
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-06-10 09:42:58 +0800

    updated

--- Inheritance.py
+++ Inheritance.py
@@ -1,5 +1,4 @@
 from Class_Calculator import Calculator
-from User_Interface import UserInterface
 
 class InheritCalculator(Calculator):
     def power(self, num1, num2):
@@ -7,16 +6,3 @@ class InheritCalculator(Calculator):
     
     def square_root(self, num1):
         return num1 ** 0.5
-    
-class InheritUI(UserInterface):
-    def try_again(self):
-        while True:
-            TA = input("\n\033[96mDo you want to try again? 'y' or 'n': ")
-            if TA in ["y", "n"]:
-
-                if TA == 'y':
-                    continue
-
-            else:
-                print('\nThank you!')
-                break
